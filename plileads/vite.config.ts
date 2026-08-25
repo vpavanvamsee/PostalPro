2c48bdea6d048b2fd6479565e33d387870e7772b
vpavanvamsee <72819712+vpavanvamsee@users.noreply.github.com>
2026-08-26 01:02:39 +0530

    Update vite.config.ts

--- plileads/vite.config.ts
+++ plileads/vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/plileads/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
